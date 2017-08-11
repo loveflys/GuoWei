fd7193abd48694f3135c987e6b6d749d77af5ece
陈安一 <baozi@chenanyi.com>
2017-08-11 21:34:57 +0800

    updatePayResult

--- guowei-manager/guowei-manager-web/src/main/java/com/guowei/controller/WeChatController.java
+++ guowei-manager/guowei-manager-web/src/main/java/com/guowei/controller/WeChatController.java
@@ -1,5 +1,11 @@
 package com.guowei.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -120,7 +126,7 @@ public class WeChatController {
 	 * @param request
 	 * @param response
 	 */
-	@RequestMapping(value = "/payresult")
+	@RequestMapping(value = "/payresult_delete")
 	public void payresult(HttpServletRequest request, HttpServletResponse response) {
 		try {
 			synchronized (this) {
@@ -170,5 +176,108 @@ public class WeChatController {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
+	}
+	/**
+	 * 微信通知支付结果的回调地址，notify_url
+	 *
+	 * @param request
+	 * @param response
+	 */
+	@RequestMapping(value = "/payresult")
+	public void paynotify (HttpServletRequest request, HttpServletResponse response){
+		try {
+			//读取参数  
+	        InputStream inputStream ;  
+	        StringBuffer sb = new StringBuffer();  
+	        inputStream = request.getInputStream();  
+	        String s ;  
+	        BufferedReader in = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));  
+	        while ((s = in.readLine()) != null){  
+	            sb.append(s);  
+	        }  
+	        in.close();  
+	        inputStream.close();  
+	  
+	        Map<String, String> m = PayUtils.parseRequestXmlToMap(request);
+	          
+	        //过滤空 设置 TreeMap  
+	        SortedMap<Object,Object> packageParams = new TreeMap<Object,Object>();        
+	        Iterator it = m.keySet().iterator();  
+	        while (it.hasNext()) {  
+	            String parameter = (String) it.next();  
+	            String parameterValue = m.get(parameter);  
+	              
+	            String v = "";  
+	            if(null != parameterValue) {  
+	                v = parameterValue.trim();  
+	            }  
+	            packageParams.put(parameter, v);  
+	        }  
+	        //判断签名是否正确  
+	        if(SignUtils.checkSign(m, Constant.KEY)) {  
+	        	GwOrder order = orderService.getGwOrderById(Long.parseLong(request.getParameter("id")));
+	            //------------------------------  
+	            //处理业务开始  
+	            //------------------------------  
+	            String resXml = "";  
+	            if("SUCCESS".equals((String)packageParams.get("result_code"))){  
+	                // 这里是支付成功  
+	                //////////执行自己的业务逻辑////////////////  
+	                String mch_id = (String)packageParams.get("mch_id");  
+	                String openid = (String)packageParams.get("openid");  
+	                String is_subscribe = (String)packageParams.get("is_subscribe");  
+	                String out_trade_no = (String)packageParams.get("out_trade_no");  
+	                  
+	                String total_fee = (String)packageParams.get("total_fee");  
+	                  
+	                System.out.println("mch_id:"+mch_id);  
+	                System.out.println("openid:"+openid);  
+	                System.out.println("is_subscribe:"+is_subscribe);  
+	                System.out.println("out_trade_no:"+out_trade_no);  
+	                System.out.println("total_fee:"+total_fee);  
+	                  
+	                //////////执行自己的业务逻辑////////////////  
+	                  
+	                System.out.println("支付成功"); 
+	                order.setStatus(Byte.parseByte("2"));
+					int status = orderService.editGwOrder(order);
+					
+					if (status == 1) {
+						System.out.println("支付成功.修改订单状态成功");
+					} else {
+						System.out.println("支付成功.修改订单状态失败");
+					}
+	                //通知微信.异步确认成功.必写.不然会一直通知后台.八次之后就认为交易失败了.  
+	                resXml = "<xml>" + "<return_code><![CDATA[SUCCESS]]></return_code>"  
+	                        + "<return_msg><![CDATA[OK]]></return_msg>" + "</xml> ";  
+	                  
+	            } else {  
+	            	System.out.println("支付失败,错误信息：" + packageParams.get("err_code"));  
+	            	order.setStatus(Byte.parseByte("3"));
+					int status = orderService.editGwOrder(order);
+					
+					if (status == 1) {
+						System.out.println("支付失败.修改订单状态成功");
+					} else {
+						System.out.println("支付失败.修改订单状态失败");
+					}
+	                resXml = "<xml>" + "<return_code><![CDATA[FAIL]]></return_code>"  
+	                        + "<return_msg><![CDATA[报文为空]]></return_msg>" + "</xml> ";  
+	            }  
+	            //------------------------------  
+	            //处理业务完毕  
+	            //------------------------------  
+	            BufferedOutputStream out = new BufferedOutputStream(  
+	                    response.getOutputStream());  
+	            out.write(resXml.getBytes());  
+	            out.flush();  
+	            out.close();  
+	        } else{  
+	        	System.out.println("通知签名验证失败");  
+	        }  
+		} catch (Exception e) {
+			System.out.println("微信支付回调失败==>"+e.getMessage());
+		}
+		
 	}
 }
