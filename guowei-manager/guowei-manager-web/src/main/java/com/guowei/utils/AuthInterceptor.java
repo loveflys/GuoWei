10398af215b8e7a47267325e8cd1ab5210011556
陈安一 <baozi@chenanyi.com>
2017-09-17 17:05:56 +0800

    update

--- guowei-manager/guowei-manager-web/src/main/java/com/guowei/utils/AuthInterceptor.java
+++ guowei-manager/guowei-manager-web/src/main/java/com/guowei/utils/AuthInterceptor.java
@@ -43,51 +43,52 @@ public class AuthInterceptor implements HandlerInterceptor {
 		}
 		log.debug("发起请求==>" + url + "||" + request.getMethod() + "||" + params.toString());
 		System.out.println("发起请求==>" + url + "||" + request.getMethod() + "||" + params.toString());
-		GwManager manager = (GwManager) request.getSession().getAttribute(Constants.CURRENT_USER);
-		System.out.println(request.getSession().getAttribute(Constants.CURRENT_USER));
-		if (handler.getClass().isAssignableFrom(HandlerMethod.class)) {
-			AuthPassport authPassport = ((HandlerMethod) handler).getMethodAnnotation(AuthPassport.class);
-
-			// 没有声明需要权限
-			if (authPassport == null)
-				return true;
-			else {
-				// 在这里实现自己的权限验证逻辑
-				if (request.getSession().getAttribute(Constants.CURRENT_USER) != null) {// 如果验证成功返回true（这里直接写false来模拟验证失败的处理）
-					boolean res;
-					switch(authPassport.level()) {
-						case 1: 
-							res = true;
-							break;
-						case 2: 
-							res = Arrays.asList(2, 3, 4).contains(manager.getLevel().intValue());
-							break;
-						case 3: 
-							res = Arrays.asList(3, 4).contains(manager.getLevel().intValue());
-							break;
-						case 4: 
-							res = manager.getLevel().intValue() == 4;
-							break;
-						case 5: 
-							res = Arrays.asList(4, 5).contains(manager.getLevel().intValue());
-							break;
-						case 6: 
-							res = Arrays.asList(4, 5, 6).contains(manager.getLevel().intValue());
-							res = true;
-							break;
-						default: 
-							res = true;
-							break;
-					}
-					return res;
-				} else { // 如果验证失败
-					// 返回到登录界面
-					response.sendRedirect("login");
-					return false;
-				}
-			}
-		} else
-			return true;
+//		GwManager manager = (GwManager) request.getSession().getAttribute(Constants.CURRENT_USER);
+//		System.out.println(request.getSession().getAttribute(Constants.CURRENT_USER));
+//		if (handler.getClass().isAssignableFrom(HandlerMethod.class)) {
+//			AuthPassport authPassport = ((HandlerMethod) handler).getMethodAnnotation(AuthPassport.class);
+//
+//			// 没有声明需要权限
+//			if (authPassport == null)
+//				return true;
+//			else {
+//				// 在这里实现自己的权限验证逻辑
+//				if (request.getSession().getAttribute(Constants.CURRENT_USER) != null) {// 如果验证成功返回true（这里直接写false来模拟验证失败的处理）
+//					boolean res;
+//					switch(authPassport.level()) {
+//						case 1: 
+//							res = true;
+//							break;
+//						case 2: 
+//							res = Arrays.asList(2, 3, 4).contains(manager.getLevel().intValue());
+//							break;
+//						case 3: 
+//							res = Arrays.asList(3, 4).contains(manager.getLevel().intValue());
+//							break;
+//						case 4: 
+//							res = manager.getLevel().intValue() == 4;
+//							break;
+//						case 5: 
+//							res = Arrays.asList(4, 5).contains(manager.getLevel().intValue());
+//							break;
+//						case 6: 
+//							res = Arrays.asList(4, 5, 6).contains(manager.getLevel().intValue());
+//							res = true;
+//							break;
+//						default: 
+//							res = true;
+//							break;
+//					}
+//					return res;
+//				} else { // 如果验证失败
+//					// 返回到登录界面
+//					response.sendRedirect("login");
+//					return false;
+//				}
+//			}
+//		} else
+//			return true;
+		return true;
 	}
 
 	@Override
